ebd445840edb087ea19347393a01ebfe86e09c0f
VinceDriesen <vince.driesen@student.uhasselt.be>
2026-03-24 15:10:55 +0100

    Made the user model class

--- parking/src/main/java/com/bip/parking/database/models/User.java
+++ parking/src/main/java/com/bip/parking/database/models/User.java
@@ -1,4 +1,27 @@
 package com.bip.parking.database.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String numberPlate;
+
+    private boolean admin;
 }
